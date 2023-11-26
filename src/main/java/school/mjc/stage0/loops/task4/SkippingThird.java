5741b0083787aeb0b0c48c3aea18354ebb756943
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-11-26 20:49:51 +0300

    Finish : Module #4.2. Task #4. Loop Keywords

--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -3,5 +3,23 @@ package school.mjc.stage0.loops.task4;
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
 
+        int count = 0;
+
+        for( int i = 1; i <= lastPrinted; i++) {
+
+            count++;
+
+            if(count == 3) {
+
+                count = 0;
+
+                continue;
+
+            }
+
+            System.out.println(i);
+
+        }
+
     }
 }
